3c6b9a8c1cfd4dba343ce0990eb46f11e2cdada3
ffrontera <ffrontera@live.com>
2024-06-07 22:59:15 -0300

    Se agregaron las validaciones para agendar consultas

--- src/main/java/med/voll/api/domain/consulta/AgendaDeConsultaService.java
+++ src/main/java/med/voll/api/domain/consulta/AgendaDeConsultaService.java
@@ -1,11 +1,14 @@
 package med.voll.api.domain.consulta;
 
+import med.voll.api.domain.consulta.validaciones.ValidadorDeConsultas;
 import med.voll.api.domain.medico.Medico;
 import med.voll.api.domain.medico.MedicoRepository;
 import med.voll.api.domain.paciente.PacienteRepository;
 import med.voll.api.infra.errores.ValidacionDeIntegridad;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AgendaDeConsultaService {
@@ -16,6 +19,8 @@ public class AgendaDeConsultaService {
     private MedicoRepository medicoRepository;
     @Autowired
     private ConsultaRepository consultaRepository;
+    @Autowired
+    List<ValidadorDeConsultas> validadores;
 
     public void agendar(DatosAgendarConsulta datos) {
         if (pacienteRepository.findById(datos.idPaciente()).isPresent()){
@@ -25,6 +30,8 @@ public class AgendaDeConsultaService {
         if (datos.idMedico() != null && medicoRepository.existsById(datos.idMedico())) {
             throw new ValidacionDeIntegridad("este id de medico no fue encontrado")
         }
+
+        validadores.forEach(v -> v.validar(datos));
 
         var paciente = pacienteRepository.findById(datos.idPaciente()).get();
         var medico = seleccionarMedico(datos);
